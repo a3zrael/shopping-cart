0a858d30fc24d6e3a468bc6be3e3cc789f55425a
Andrey_Marusov <marusov140@gmail.com>
2023-08-09 22:00:13 +0300

    minor fix

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ let basket = JSON.parse(localStorage.getItem("dataItems")) || []
 const generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x) => {
         let { id, name, price, desc, image } = x;
+        let search = basket.find((x) => x.id === id) || []
         return `
         <div id=product-id-${id} class="item">
         <img width="220" src=${image} alt="">
@@ -44,7 +45,7 @@ const generateShop = () => {
             <h2>${price}$</h2>
             <div class="buttons">
               <i onClick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id} class="quantity">0</div>
+              <div id=${id} class="quantity">${search.item === undefined ? 0 : search.item}</div>
               <i onClick="increment(${id})" class="bi bi-plus-lg"></i>
             </div>
           </div>
@@ -68,9 +69,9 @@ let increment = (id) => {
         search.item += 1;
     }
 
-    localStorage.setItem("dataItems", JSON.stringify(basket))
-
     update(selectedItem.id);
+    
+    localStorage.setItem("dataItems", JSON.stringify(basket))
 }
 
 let decrement = (id) => {
@@ -78,14 +79,17 @@ let decrement = (id) => {
 
     let search = basket.find((x) => x.id === selectedItem.id)
 
-    if (search.item === 0) return
+    if (search === undefined) return
+    else if (search.item === 0) return
     else {
         search.item -= 1;
     }
 
-    localStorage.setItem("dataItems", JSON.stringify(basket))
-
     update(selectedItem.id);
+    
+    basket = basket.filter((el) => el.item !== 0);
+    
+    localStorage.setItem("dataItems", JSON.stringify(basket))
 }
 
 let update = (id) => {
@@ -99,5 +103,6 @@ let calc = () => {
     cartIcon.innerHTML = basket.map((el) => el.item).reduce((x, y) => x + y, 0)
 }
 
+calc()
 
 generateShop()
